f9e2847cce3990a124a6f247daaa59874d6e09b4
Nakshatra Gupta <nakshatra.nsb@gmail.com>
2023-03-05 19:53:03 +0000

    Completed Project

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db, load_job_from_db
+from flask import Flask, render_template, jsonify, request
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 
 
@@ -31,6 +31,12 @@ def show_job(id):
   if not job:
     return "Not Found", 404
   return render_template("jobpage.html", job=job)
+
+@app.route("/job/<id>/apply", methods =['post'])
+def apply_to_job(id):
+  data = request.form
+  add_application_to_db(id,data)
+  return render_template('application_submitted.html', application = data)
   
 
 
